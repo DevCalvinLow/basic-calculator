6a7500262761555bac09ea34dbf7a0789025e30f
DevCalvinLow <devcalvinlow@gmail.com>
2024-06-22 18:30:59 -0700

    Adding files created

---
+++ 1-Project-1-Calculator/js/main.js
@@ -0,0 +1,16 @@
+function addNums(num1, num2){ 
+    return num1 + num2
+}
+function subtractNums (num1, operator, num2){ 
+    return num1 - num2
+}
+function multiplyNums (num1, operator, num2){ 
+    return num1 * num2
+}
+function divideNums (num1, operator, num2){ 
+    return num1 / num2
+}
+
+function operate(operator, num1, num2)
+
+
